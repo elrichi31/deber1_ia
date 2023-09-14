bfb79154fe0857b5fafb6c4cb9ec9dd5adcc555b
Nicolas <Nicolas>
2023-09-14 09:42:43 -0500

    Avances locos (Final final) juebebes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Nicolás Moina: Comprobación de codigo
+Nicolás Moina(00321192): Impresión de los nodos fronteras y nodos hijos con sus respectivos costos, en términos de las ciudades visitadas en el orden de la ruta y costo total de la ruta. 
 
-David Reinoso: Análisis 
+Diego Reinoso(00214020): Análisis e implementación inicial del algoritmo de Dijkstra
 
-Gabriel Lara: Más Análisis demente 
+Gabriel Lara(00215784): Creación de la clase Grafo y sus métodos, implementación de la función de la ruta mas corta 
 
-IA: todo :) 
+
--- deber1.py
+++ deber1.py
@@ -1,4 +1,4 @@
-def dijkstra(Grafo, salida, llegada):
+def dijkstra(Grafo, salida):
     dist, prev = {}, {}
     result = []
 
@@ -21,26 +21,45 @@ def dijkstra(Grafo, salida, llegada):
 
     return result, dist, prev
 
-def ruta_mas_corta(prev, inicio, fin):
+def ruta_mas_corta(prev, dist, inicio, fin):
     ruta = [fin]
+    distancias = [dist[fin]]
     while ruta[-1] != inicio:
         ruta.append(prev[ruta[-1]])
+        distancias.append(dist[ruta[-1]])
     ruta.reverse()
-    return ruta
+    distancias.reverse()
+    return ruta, distancias
+
 
 grafo = {
-    'a': {'b': 4, 'c': 3},
-    'b': {'d': 5},
-    'c': {'b': 2, 'd': 3, 'e': 6},
-    'd': {'f': 5, 'e': 1},
-    'e': {'g': 5},
-    'g': {'z': 4},
-    'f': {'g': 2, 'z': 7},
-    'z': {}
+    'Ellensburg': {'Pendleton': 168, 'Spokane': 175},
+    'Pendleton': {'Spokane': 200, 'Missoula': 356, 'Ellensburg': 168},
+    'Spokane': {'Bonners Ferry': 112, 'Missoula': 199, 'Ellensburg': 175, 'Pendleton': 200},
+    'Bonners Ferry': {'West Glacier': 176, 'Missoula': 249, 'Spokane': 112},
+    'West Glacier': {'Missoula': 151, 'Bonners Ferry': 176, 'Helena': 243, 'Great Falls': 211, 'Havre': 231},
+    'Missoula': {'West Glacier': 151, 'Bonners Ferry': 249, 'Spokane': 199, 'Pendleton': 356, 'Helena': 111, 'Butte': 119},
+    'Butte': {'Missoula': 119, 'Helena': 65},
+    'Helena': {'Butte': 65, 'Missoula': 111, 'West Glacier': 243, 'Great Falls': 91},
+    'Great Falls': {'Helena': 91, 'West Glacier': 211, 'Havre': 115},
+    'Havre': {'Great Falls': 115, 'West Glacier': 231},
 }
 
-inicio = 'a'
-destino = 'z'
-s, distancia, previos = dijkstra(grafo, inicio, destino)
-ruta = ruta_mas_corta(previos, inicio, destino)
-print(f"La ruta más corta desde {inicio} hasta {destino} es: {' -> '.join(ruta)} con una distancia de {distancia[destino]}")
+def imprimir_ruta_y_exploracion(grafo, ruta, distancias_ruta):
+    for i in range(1, len(ruta)+1):
+        print("Frontera={} (peso = {} km); Explored={}".format(ruta[i-1], distancias_ruta[i-1], ', '.join(grafo[ruta[i-1]].keys())))
+
+inicio = 'Ellensburg'
+destino = 'Havre'
+s, distancia, previos = dijkstra(grafo, inicio)
+ruta, distancias_ruta = ruta_mas_corta(previos, distancia, inicio, destino)
+
+print(f"La ruta desde {inicio} hasta {destino} es:")
+imprimir_ruta_y_exploracion(grafo, ruta, distancias_ruta)
+for i in range(len(ruta)):
+    if i == len(ruta) - 1:
+        print(f"{ruta[i]}")
+    else:
+        print(f"{ruta[i]} ({distancias_ruta[i+1] - distancias_ruta[i]} km) -> ", end="")
+print(f"con una distancia total de {distancia[destino]} km.")
+
