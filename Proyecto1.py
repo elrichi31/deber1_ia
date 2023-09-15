57ca0f57f6048960ecac135ef64fa429f3f158ce
Gabriel Lara <gabriel.lara4@hotmail.com>
2023-09-14 22:55:52 -0500

    CambiosFinales

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,37 +1,48 @@
 def dijkstra(Grafo, salida):
+    # Inicialización de variables
     dist, prev = {}, {}
     result = []
-
+    
+    # Asignación de valores iniciales
     for vertice in Grafo:
         dist[vertice] = float("inf")
         prev[vertice] = None
     dist[salida] = 0
+    
+    
+  # Creación de lista ElementosVertices con todos los vértices del grafo
+    ElementosVertices = [vertice for vertice in Grafo]
 
-    Q = [vertice for vertice in Grafo]
-
-    while Q:
-        u = min(Q, key=dist.get)
-        Q.remove(u)
+    while ElementosVertices:
+        # Selecciona el vértice con la menor distancia
+        u = min(ElementosVertices, key=dist.get)
+        ElementosVertices.remove(u)
         result.append(u)
 
+        # Actualiza las distancias de los vecinos del vértice seleccionado
         for vecino in Grafo[u]:
-            if vecino in Q and dist[vecino] > dist[u] + Grafo[u][vecino]:
+            if vecino in ElementosVertices and dist[vecino] > dist[u] + Grafo[u][vecino]:
                 dist[vecino] = dist[u] + Grafo[u][vecino]
                 prev[vecino] = u
 
     return result, dist, prev
 
 def ruta_mas_corta(prev, dist, inicio, fin):
+    # Inicialización de variables   
     ruta = [fin]
     distancias = [dist[fin]]
+    # Bucle para encontrar la ruta más corta
     while ruta[-1] != inicio:
         ruta.append(prev[ruta[-1]])
         distancias.append(dist[ruta[-1]])
+        
+    # Invierte las listas para que estén en el orden correcto
     ruta.reverse()
     distancias.reverse()
     return ruta, distancias
 
 
+#Representacion de las cuidades como nodos con sus respectivos pesos
 grafo = {
     'Ellensburg': {'Pendleton': 168, 'Spokane': 175},
     'Pendleton': {'Spokane': 200, 'Missoula': 356, 'Ellensburg': 168},
@@ -45,6 +56,8 @@ grafo = {
     'Havre': {'Great Falls': 115, 'West Glacier': 231},
 }
 
+
+# Imprime la información de la ruta y los nodos explorados
 def imprimir_ruta_y_exploracion(grafo, ruta, distancias_ruta):
     for i in range(1, len(ruta)+1):
         print("Frontera={} (peso = {} km); Explored={}".format(ruta[i-1], distancias_ruta[i-1], ', '.join(grafo[ruta[i-1]].keys())))
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Nicolás Moina(00321192): Impresión de los nodos fronteras y nodos hijos con sus respectivos costos, en términos de las ciudades visitadas en el orden de la ruta y costo total de la ruta. 
+
+Diego Reinoso(00214020): Análisis e implementación inicial del algoritmo de Dijkstra
+
+Gabriel Lara(00215784): Creación del modelo de las ciudades en un grafo y  implementación de la función de la ruta mas corta y comentarios del código
+
+
